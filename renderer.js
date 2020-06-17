9594a147f055d86d8a8adae9e3ee75f04b00700d
Trinh Dinh Viet <trinh.dinh.viet@sun-asterisk.com>
2020-06-17 16:00:24 +0700

    ff

--- renderer.js
+++ renderer.js
@@ -4,3 +4,9 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+update_current_branch();
+
+
+function update_current_branch() {
+  document.getElementById('current_branch').innerHTML = 'testttttttttt'
+}
